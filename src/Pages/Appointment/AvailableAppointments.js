f3b63f444e5352b3fd1ca237805d10060f670f61
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-05-19 13:44:15 +0600

    first commit

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -8,7 +8,7 @@ const AvailableAppointments = ({ date }) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
